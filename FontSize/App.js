a5bd003ff8b2c82d23d47f0fdb7e56986cad5636
HariPriya <haripriyaatluri2@gmail.com>
2025-06-26 11:00:55 +0530

    Add files via upload

---
+++ FontSize/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmojiDisplay from './EmojiDisplay/EmojiDisplay';
+
+function App() {
+  return (
+    <div className="App">
+     <EmojiDisplay/>
+    </div>
+  );
+}
+
+export default App;
